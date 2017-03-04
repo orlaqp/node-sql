8b2a24e7d85ae5c0bcfcae27cc0357b294dba7e0
Orlando Quero <orlaqp@gmail.com>
2017-03-04 08:34:16 -0500

    ready to test statements

---
+++ app.js
@@ -0,0 +1,43 @@
+var Connection = require('tedious').Connection;  
+    var config = {  
+        userName: 'username',  
+        password: 'password',  
+        server: 'server',  
+        // When you connect to Azure SQL Database, you need these next options.  
+        options: {encrypt: true, database: 'db'}  
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        if (err) return console.error(err);
+        // If no error, then good to proceed.  
+        console.log("Connected");  
+        executeStatement();  
+    });  
+
+    var Request = require('tedious').Request;  
+    var TYPES = require('tedious').TYPES;  
+
+    function executeStatement() {  
+        request = new Request("<statement>", function(err) {  
+        if (err) {  
+            console.log(err);}  
+        });  
+        var result = "";  
+        request.on('row', function(columns) {  
+            columns.forEach(function(column) {  
+              if (column.value === null) {  
+                console.log('NULL');  
+              } else {  
+                result+= column.value + " ";  
+              }  
+            });  
+            console.log(result);  
+            result ="";  
+        });  
+
+        request.on('done', function(rowCount, more) {  
+        console.log(rowCount + ' rows returned');  
+        });  
+        connection.execSql(request);  
+    }
+
